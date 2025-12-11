f9d706e8137268499bf2ee9b5b2acfacb4244c73
Thomas <Thomas.Bratvold@gmail.com>
2025-12-11 03:50:37 -0600

    Ants walk randomly while avoiding trail. Failed.

--- src/ant.ts
+++ src/ant.ts
@@ -1,34 +1,167 @@
+const TAU = Math.PI * 2;
+
 export type Ant = {
     position: {x: number, y: number};
     direction: {x: number, y: number};
     speed: number;
 }
 
+export type Segment = {
+    start: number;
+    end: number;
+}
+
+export type Coordinate = {
+    x: number;
+    y: number;
+}
+
 export function createAnt(x:number, y:number): Ant{
     return {
         position: {x: x, y: y},
         direction: {x: 1, y: 0},
-        speed: 0.1
+        speed: 0.2
     };
 }
 
-export function updateDirection(ant: Ant): void{
+export function updateDirection(ant: Ant, avoidedCoordinates: Coordinate[]): void{
+    // avoidedCoordinates = avoidedCoordinates.filter(c => Math.hypot(ant.position.x - c.x, ant.position.y - c.y) < 20)
+    let avoidedCoordinatesPrime = avoidedCoordinates.filter(c => {
+        const visionCircleSize:number = 20;
+        let visionCircleCenterX:number =  ant.position.x + visionCircleSize*ant.direction.x;
+        let visionCircleCenterY:number =  ant.position.y + visionCircleSize*ant.direction.y;
+        let dx:number = visionCircleCenterX - c.x;
+        let dy:number = visionCircleCenterY - c.y;
+        return dx < visionCircleSize && dy < visionCircleSize;
+    })
+    // console.log(avoidedCoordinates)
+    if(avoidedCoordinatesPrime.length < 10){
+        return;
+    }
+    
+    let avoidedSegments:Segment[] = getAvoidedSegments(ant, avoidedCoordinatesPrime);
+    // avoidedSegments.push({
+    //     start: Math.atan2(ant.direction.y, ant.direction.x) + (20 * (Math.PI/180)),
+    //     end: Math.atan2(ant.direction.y, ant.direction.x) - (20 * (Math.PI/180))});
+    avoidedSegments = normaliseSegments(avoidedSegments);
+    avoidedSegments = mergeSegments(avoidedSegments);
 
-    let length:number = 0;
-    let x: number = 0;
-    let y: number = 0;
-    while(length === 0){
-        x = Math.random() * 2 - 1;
-        y = Math.random() * 2 - 1;
-        length = Math.hypot(x,y);
+    const totalAvoided:number = avoidedSegments.reduce((sum, seg) => sum + (seg.end - seg.start), 0);
+    const totalAllowed:number = TAU - totalAvoided;
+
+    if(totalAllowed === 0){
+        // ant.speed = 0;
+        let angle:number = Math.atan2(ant.direction.y, ant.direction.x)
+        angle += (90 * (Math.PI/180))
+        ant.direction = {x: Math.cos(angle), y: Math.sin(angle)};
+        console.log(totalAllowed*180/Math.PI)
+        return;
+        // return updateDirection(ant, avoidedCoordinates);
+    }
+    // console.log(totalAvoided*180/Math.PI)
+
+    let angle:number = Math.random() * totalAllowed;
+    for(const seg of avoidedSegments){
+        if(angle < seg.start){
+            break;
+        } else {
+            angle += seg.end - seg.start;
+        }
     }
 
-    ant.direction = {x: x/length, y: y/length};
+    ant.direction = {x: Math.cos(angle), y: Math.sin(angle)};
 }
 
 export function step(ant: Ant, delta: number): void{
-
     ant.position.x += ant.speed * ant.direction.x * delta;
     ant.position.y += ant.speed * ant.direction.y * delta;
-
 }
+
+function getAvoidedSegments(ant:Ant , avoidedCoordinates: Coordinate[]): Segment[]{
+    const segments:Segment[] = [];
+    const range = 5 * (Math.PI/180) // +/- 5 degrees
+    for (const coord of avoidedCoordinates) {
+        const dx = coord.x - ant.position.x;
+        const dy = coord.y - ant.position.y;
+        const angle = Math.atan2(dy, dx);
+        segments.push({start: angle - range, end: angle + range})
+    }
+    return segments;
+}
+
+function normaliseSegments(segments:Segment[]): Segment[]{
+    const normalised: Segment[] = [];
+    for (const seg of segments) {
+
+        let start = (((seg.start % TAU) + TAU) % TAU);
+        let end = (((seg.end % TAU) + TAU) % TAU);
+        
+        if (start < end){
+            normalised.push({start: start,end: end});
+        } else {
+            normalised.push({start: start,end: TAU});
+            normalised.push({start: 0,end: end});
+        }
+    }
+    return normalised;
+}
+
+function mergeSegments(segments:Segment[]): Segment[]{
+    // Merge overlapping segments
+    segments.sort((a,b) => a.start - b.start);
+
+    const merged: Segment[] = [];
+    for(const seg of segments){
+        if(merged.length === 0){
+            merged.push({...seg})
+            continue;
+        }
+
+        const last = merged[merged.length -1];
+        if(seg.start <= last.end){
+            last.end = Math.max(seg.end, last.end);
+        } else {
+            merged.push({...seg});
+        }
+    }
+
+    return merged;
+}
+
+// function getAvoidedAngleSegments(segments:Segment[]): Segment[]{
+//     // Make all segments go from 0-TAU and split segments that 'go over' TAU (eg. 1.5pi-0.5pi)
+//     const normalised: Segment[] = [];
+//     for (const seg of segments) {
+
+//         let start = (((seg.start % TAU) + TAU) % TAU);
+//         let end = (((seg.end % TAU) + TAU) % TAU);
+        
+//         if (start < end){
+//             normalised.push({start: start,end: end});
+//         } else {
+//             normalised.push({start: start,end: TAU});
+//             normalised.push({start: 0,end: end});
+//         }
+//     }
+
+//     // Merge overlapping segments
+//     normalised.sort((a,b) => a.start - b.start);
+
+//     const merged: Segment[] = [];
+//     for(const seg of normalised){
+
+//         if(merged.length === 0){
+//             merged.push({...seg})
+//             continue;
+//         }
+
+//         const last = merged[merged.length -1];
+//         if(seg.start <= last.end){
+//             last.end = Math.max(seg.end, last.end);
+//         } else {
+//             merged.push({...seg});
+//         }
+//     }
+
+//     return merged;
+// }
--- src/map.ts
+++ src/map.ts
@@ -1,16 +1,22 @@
 import renderer from "./renderer";
-import type { Ant } from "./ant";
+import type { Ant, Coordinate } from "./ant";
 import { updateDirection, step, createAnt } from "./ant";
 
 
 const speed: number = 0.03;
 let x: number = 0;
-let ant:Ant;
+let ants:Ant[] = [];
 let count:number = 0;
+let squares:Square[] = [];
 renderer(
     ({ canvas, ctx }) => {
         const { width, height } = canvas;
-        ant = createAnt(width/2, height/2);
+        for(let i = 1; i < 4; i++){
+            // ants.push(createAnt(width * Math.random(), height * Math.random()));
+            ants.push(createAnt(width/2, height/2));
+        }
+        
+        // squares = createSquares(500, width, height);
     },
     (delta, { canvas, ctx }) => {
         const { width, height } = canvas;
@@ -19,24 +25,63 @@ renderer(
         ctx.fillStyle = `rgb(40,40,60)`;
         ctx.fillRect(0, 0, width, height);
 
-        count++;
-        count = count % 100;
-        if(count === 0){
-            updateDirection(ant);
+        
+        const ageLimit:number = 1000;
+        squares = squares.filter(s => s.age < ageLimit);
+        for(const square of squares){
+            ctx.fillStyle = `rgba(0, 120, 120, ${(ageLimit - square.age) / ageLimit})`;
+            // ctx.fillRect(square.position.x - square.size/2, square.position.y - square.size/2, square.size, square.size)
+            ctx.beginPath();
+            ctx.arc(
+                square.position.x,      // center X
+                square.position.y,      // center Y
+                square.size / 2,        // radius
+                0,
+                Math.PI * 2             // full circle
+            );
+            ctx.fill();
+            square.age += 1;
         }
 
-        step(ant, delta);
 
-        // Let ant 'wrap' around the canvas
-        ant.position = {
-            x: ((ant.position.x % width) + width) % width, 
-            y: ((ant.position.y % height) + height) % height
-        };
+        for (const ant of ants){
+            updateDirection(ant, squares.map((s) => s.position));
+            // console.log(ant);
+
+            squares.push({position: ant.position, size: 5, age: 0});
+
+            step(ant, delta);
+            // Let ant 'wrap' around the canvas
+            ant.position = {
+                x: ((ant.position.x % width) + width) % width, 
+                y: ((ant.position.y % height) + height) % height
+            };
+            
+            ctx.fillStyle = `rgb(90,20,20)`;
+            ctx.fillRect(ant.position.x-5, ant.position.y-5, 10, 10);
+            ctx.fillStyle = `rgb(20,120,20)`;
+            ctx.fillRect((ant.position.x-1) + (ant.direction.x * 5), (ant.position.y-1) + (ant.direction.y * 5), 2, 2);
+        }
+
         
-        ctx.fillStyle = `rgb(90,20,20)`;
-        ctx.fillRect(ant.position.x-5, ant.position.y-5, 10, 10);
-        ctx.fillStyle = `rgb(20,255,20)`;
-        ctx.fillRect((ant.position.x-1) + (ant.direction.x * 5), (ant.position.y-1) + (ant.direction.y * 5), 2, 2);
 
     }
 );
+
+type Square = {
+    position: Coordinate;
+    size: number;
+    age: number;
+}
+
+function createSquares(n:number, maxX:number, maxY:number): Square[]{
+    const squares:Square[] = [];
+    for(let i = 0; i < n; i++){
+        squares.push({
+            position: {x:Math.random() * maxX,y:Math.random() * maxY},
+            size: 5,
+            age: 0
+        });
+    }
+    return squares;
+}
